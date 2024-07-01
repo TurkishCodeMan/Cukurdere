738f7fc0866caa404b5286847fdb684eeeb8ee58
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2024-07-01 21:27:11 +0300

    --

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,20 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
 export default function Head() {
+  const t = useTranslations('head');
+
   return (
     <>
-      <title>Çukurdere Sondaj - Derinlerdeki Gücünüz !</title>
+      <title>{t('title')}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Derinlerdeki Gücünüz !" />
-      <meta name="keywords" content="sondaj, maden arama, jeotermal sondaj, su sondajı, profesyonel çözümler,türkiye'nin en büyük sondaj firmaları" />
-      <meta name="author" content="Çukurdere Sondaj" />
+      <meta name="description" content={t('description')} />
+      <meta name="keywords" content={t('keywords')} />
+      <meta name="author" content={t('author')} />
       <link rel="icon" href="/0000246.png" />
-      <link rel="canonical" href="https://www.cukurderesondaj.com.tr/home" />
-      <meta name="google-site-verification" content="GVV3xd9PB7n23bvfDQ4PEb1Lmk3GMpOM_bUhDyDEPog" />
+      <link rel="canonical" href={t('canonical')} />
+      <meta name="google-site-verification" content={t('googleVerification')} />
     </>
   );
 }
