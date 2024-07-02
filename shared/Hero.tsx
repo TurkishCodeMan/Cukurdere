d3e36f5e3624ca76f691327b88a1d8e74409ccba
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2024-07-02 21:27:29 +0300

    updatge

--- shared/Hero.tsx
+++ shared/Hero.tsx
@@ -124,33 +124,38 @@ export default function Hero() {
           </div>
         </SwiperSlide>
         <SwiperSlide
-          className="bg-cover bg-center md:bg-center min-h-screen flex items-center justify-center "
-          style={{
-            backgroundImage:
-              "linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('/a3.jpeg')",
-          }}
-        >
-          <div className="md:flex md:flex-row flex flex-col ">
-            <div
-              className="md:w-1/2 w-full h-screen bg-black bg-opacity-0 flex items-center justify-center"
-              style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 80%)" }}
-            >
-              <div className="text-center p-8 relative mr-auto">
-                <p className="text-xl font-sans opacity-80 mt-4 text-white relative z-10">
-                  <Bandrol />
-                </p>
-                <a
-                  href="/references"
-                  className="relative inline-block mt-8 mb-2 px-6 py-2 font-semibold text-white overflow-hidden group"
-                >
-                  <span className="relative z-10 text-lg">{t('references')}</span>
-                  <div className="absolute inset-0 bg-red-500 transition-all duration-200 ease-in-out clip-path-right group-hover:clip-path-full"></div>
-                </a>
-                <div className="absolute inset-0 z-0"></div>
-              </div>
-            </div>
+  className="bg-cover min-h-screen flex items-center justify-center md:bg-center"
+  style={{
+    backgroundImage:
+      "linear-gradient(to left, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url('/a3.jpeg')",
+  }}
+>
+  <div className="flex flex-col overflow-hidden">
+    <div className="md:w-1/2 w-full h-screen flex items-center justify-center">
+      <div className="text-center container p-8 relative z-30">
+        <div className="flex items-center justify-center">
+          <div className="md:flex items-center flex-col">
+            <p className="text-6xl font-thin my-2 text-white z-10 animate-fade-in">
+              <Bandrol />
+            </p>
           </div>
-        </SwiperSlide>
+        </div>
+        <a
+          href="/references"
+          className="relative inline-block mt-8 mb-2 px-6 py-2 font-semibold text-white overflow-hidden group"
+        >
+          <span className="relative z-10 text-lg">{t('references')}</span>
+          <div className="absolute inset-0 bg-red-500 transition-all duration-200 ease-in-out clip-path-right group-hover:clip-path-full"></div>
+        </a>
+        <div className="absolute inset-0 z-0"></div>
+      </div>
+    </div>
+  </div>
+  <div className="absolute bottom-0 right-0 z-20">
+    <img src="/logo_white.png" alt="Bars" className="w-32 h-auto" />
+  </div>
+</SwiperSlide>
+
     
       </Swiper>
     </section>
